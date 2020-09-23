dd8a1a4d3e93511c0eec206470d176537cf9d753
Gokulnath <gokulgokulnath96@gmail.com>
2020-09-23 10:16:52 +0530

    Completed RestTemplate Code

--- order-service/src/main/java/com/microservice/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/microservice/orderservice/service/OrderService.java
@@ -2,18 +2,35 @@ package com.microservice.orderservice.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-import com.microservice.orderservice.entity.Order;
+import com.microservice.orderservice.common.Payment;
+import com.microservice.orderservice.common.TransactionRequest;
+import com.microservice.orderservice.common.TransactionResponse;
 import com.microservice.orderservice.repository.OrderRepository;
 
 @Service
 public class OrderService {
-	
+
 	@Autowired
 	OrderRepository orderrepository;
-	
-	public Order saveOrder(Order order) {
-		return orderrepository.save(order);
+
+	@Autowired
+	RestTemplate restTemplate;
+
+	public TransactionResponse saveOrder(TransactionRequest transactionRequest) {
+
+		Payment payment = new Payment();
+		payment.setOrderId(transactionRequest.getOrder().getId());
+		payment.setAmount(transactionRequest.getOrder().getPrice());
+
+		Payment paymentResponse = restTemplate.postForObject("http://localhost:9191/payment/doPayment", payment, Payment.class);
+		orderrepository.save(transactionRequest.getOrder());
+
+		String response = paymentResponse.getPaymentStatus().equalsIgnoreCase("Success") ? "Payment Successfull, Order Placed" : "Failure in Payment" ;
+
+		return new TransactionResponse(transactionRequest.getOrder(),paymentResponse.getTransactionId(),paymentResponse.getAmount(),response);
+
 	}
 
 }
