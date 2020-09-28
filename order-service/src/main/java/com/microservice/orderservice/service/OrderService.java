d3bb886c7d71224112eea33d9bb342250512cede
Gokulnath <gokulgokulnath96@gmail.com>
2020-09-28 17:51:32 +0530

    Changes Made

--- order-service/src/main/java/com/microservice/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/microservice/orderservice/service/OrderService.java
@@ -1,6 +1,9 @@
 package com.microservice.orderservice.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,14 +12,20 @@ import com.microservice.orderservice.common.TransactionRequest;
 import com.microservice.orderservice.common.TransactionResponse;
 import com.microservice.orderservice.repository.OrderRepository;
 
+
 @Service
+@RefreshScope
 public class OrderService {
 
 	@Autowired
 	OrderRepository orderrepository;
 
 	@Autowired
+	@Lazy
 	RestTemplate restTemplate;
+	
+	@Value("${microservice.payment-service.endpoints.endpoint.uri}")
+	private String PAYMENT_URL;
 
 	public TransactionResponse saveOrder(TransactionRequest transactionRequest) {
 
@@ -24,7 +33,7 @@ public class OrderService {
 		payment.setOrderId(transactionRequest.getOrder().getId());
 		payment.setAmount(transactionRequest.getOrder().getPrice());
 
-		Payment paymentResponse = restTemplate.postForObject("http://localhost:9191/payment/doPayment", payment, Payment.class);
+		Payment paymentResponse = restTemplate.postForObject(PAYMENT_URL, payment, Payment.class);
 		orderrepository.save(transactionRequest.getOrder());
 
 		String response = paymentResponse.getPaymentStatus().equalsIgnoreCase("Success") ? "Payment Successfull, Order Placed" : "Failure in Payment" ;
